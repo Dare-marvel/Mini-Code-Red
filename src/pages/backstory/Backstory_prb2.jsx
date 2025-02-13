9334bc53012174600d25f90989efa439a28116f8
Adwait S Purao <aspurao03@gmail.com>
2025-02-14 00:52:51 +0530

    Update Backstory_prb2.jsx

--- src/pages/backstory/Backstory_prb2.jsx
+++ src/pages/backstory/Backstory_prb2.jsx
@@ -31,7 +31,7 @@ const Backstory_prb2 = () => {
         <div className="flex justify-center items-center flex-col h-screen relative">
             {/* Background Image */}
             <img
-                src="src/assets/levels/Level_2.jpg"
+                src="../../assets/levels/Level_2.jpg"
                 alt="Background"
                 className="object-cover w-full h-full absolute z-0"
             />
